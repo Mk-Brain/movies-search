02fafe5553b8f3d03aac6fc0accd93af10fa7b3c
Morand <morandkentsa9@gmail.com>
2026-08-31 21:07:43 +0100

    modification des variables d'envirronnent

--- frontend/src/hooks/usePopularMovie.tsx
+++ frontend/src/hooks/usePopularMovie.tsx
@@ -4,7 +4,7 @@ import type { Movie } from '../models/Movie';
 
 
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+const API_BASE_URL = import.meta.env.BACKEND_URL || 'http://localhost:8000';
 
 export function usePopularMovie() {
     const [movies, setMovies] = useState<Movie[]>([]);
